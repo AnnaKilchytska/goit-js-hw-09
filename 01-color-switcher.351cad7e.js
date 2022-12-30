df391a22fd4e37870a9a2cc2c30cc2cdd35c19c2
AnnaKilchytska <80330820+AnnaKilchytska@users.noreply.github.com>
2022-12-30 20:18:40 +0000

    Deploying to gh-pages from @ AnnaKilchytska/goit-js-hw-09@d843b6a78f0368d0e54981b1f9fdcccd6cd2e894 🚀

---
+++ 01-color-switcher.351cad7e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(t){t.target.setAttribute("disabled","true"),o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(e){t.removeAttribute("disabled"),clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.351cad7e.js.map
---
+++ 01-color-switcher.96a96ae7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),n=null;t.addEventListener("click",(function(t){t.target.setAttribute("disabled","true"),n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(e){t.removeAttribute("disabled"),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.96a96ae7.js.map
