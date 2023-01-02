7ccf9e88ac6160cb88239ef185e17717731879f8
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-01-02 20:21:17 +0200

    add Notifix library to 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,17 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const formEl = document.querySelector('.form');
 let position = 1;
 
 formEl.addEventListener('submit', onSubmit);
+
+Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  timeout: 5000,
+});
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -25,10 +35,13 @@ function onSubmit(e) {
     console.log(promiseComputedDelay);
     createPromise(position, promiseComputedDelay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   }
 }
