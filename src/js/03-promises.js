adab35eb7245cfa7e53fb89614b480bb665949dd
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-01-02 20:11:02 +0200

    solve 3d task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const formEl = document.querySelector('.form');
+let position = 1;
+
+formEl.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { delay, step, amount } = getFormData(e);
+
+  for (position = 0; position < amount; position++) {
+    const promiseComputedDelay = delay + step * position;
+    console.log(promiseComputedDelay);
+    createPromise(position, promiseComputedDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function getFormData(e) {
+  const delay = Number(e.target[0].value);
+  const step = Number(e.target[1].value);
+  const amount = Number(e.target[2].value);
+
+  console.log(delay, step, amount);
+
+  return { delay, step, amount };
 }
